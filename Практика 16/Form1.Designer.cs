57ee0db6a2a91a8d568ee0dedb7d46fcafbd57c8
nerhneiro <63853354+nerhneiro@users.noreply.github.com>
2020-05-21 17:04:21 +0300

    Praktika Variant 5

---
+++ Практика 16/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Практика_16
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(144, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(123, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Binary number";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(312, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(463, 22);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(144, 285);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Do";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(246, 285);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(144, 141);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(142, 104);
+            this.panel1.TabIndex = 4;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(4, 70);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(45, 21);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "16";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(4, 43);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(45, 21);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "10";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(4, 16);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(37, 21);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "8";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(433, 285);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Binary convertation";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ Практика 16/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практика_16
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        int i = 0;
+        string text1 = "";
+        private int convert_to_ten (TextBox textBox1, TextBox textBox2) 
+        {
+            int n, ten = 0, k = 0;
+            n = Convert.ToInt32(textBox1.Text);
+            n = (int)Math.Abs(n);
+            for (int i = 0; i < textBox1.Text.Length; i++)
+            {
+                if (n % 10 == 1) ten += (int)Math.Pow(2, k);
+                k++;
+                n = n / 10;
+            }
+            if (int.Parse(textBox1.Text) < 0) ten *= -1;
+            return ten;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n;
+            string eight = "", ei;
+            text1 = textBox1.Text;
+            int ten = 0;
+            try
+            {
+                if (radioButton2.Checked == true)
+                {
+                        for (int i = 0; i < textBox1.Text.Length; i++)
+                        {
+                            if (textBox1.Text[i] != '0' && textBox1.Text[i] != '1' && textBox1.Text[i] != '-') { throw new Exception(": Вы ввели не двоичное число"); }
+                        }
+                        ten = convert_to_ten(textBox1, textBox2);
+                        textBox2.Text = "Число " + textBox1.Text + " в десятичном виде = " + ten;
+                    
+                }
+                if (radioButton1.Checked == true)
+                {
+                    n = (int)Math.Abs(int.Parse(textBox1.Text));
+                    while (n > 0)
+                    {
+                        ten = ten * 10 + n % 8;
+                        n = n / 8;
+                    }
+                    n = 0;
+                    for(int i = 0; i < textBox1.Text.Length ; i++)
+                    {
+                        if(ten != 0) n = n*10 + ten % 10;
+                        ten = ten / 10;
+                    }
+                    if (int.Parse(textBox1.Text) < 0) n *= -1;
+                    textBox2.Text = "Число " + textBox1.Text + " в 8 системе счисления = " + n;
+                }
+                if(radioButton3.Checked == true)
+                {
+                    n = (int)Math.Abs(convert_to_ten(textBox1, textBox2));
+                    
+                    while (n > 0)
+                    {
+                        if (n % 16 == 10) ei = "A";
+                        else if (n % 16 == 11) ei = "B";
+                        else if (n % 16 == 12) ei = "C";
+                        else if (n % 16 == 13) ei = "D";
+                        else if (n % 16 == 14) ei = "E";
+                        else if (n % 16 == 15) ei = "F";
+                        else ei = (n % 16).ToString();
+                        eight += ei;
+                        n = n / 16;
+                    }
+                    
+                    ei = "";
+                    for(int i = eight.Length - 1; i >= 0; i--)
+                    {
+                        ei += eight[i];
+                    }
+                    if (int.Parse(textBox1.Text) < 0) ei= "-" + ei;
+                    textBox2.Text = "Число " + textBox1.Text + " в 16 системе счисления = " + ei;
+                }
+            }
+            catch (Exception err)
+            {
+                textBox2.Text = "Неправильный формат ввода " + err.Message;
+            }
+            i = 0;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (text1 != textBox1.Text) { textBox2.Text = ""; text1 = textBox1.Text; }
+        }
+    }   
+}
